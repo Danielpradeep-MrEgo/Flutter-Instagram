7cfefcb358598273c762da21fb58649f3e371851
Danny <Danielpradeep0@gmail.com>
2022-06-02 23:24:55 +0530

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/responsive/mobile_screen_layout.dart';
 import 'package:instagram/responsive/responsive_layout_screen.dart';
 import 'package:instagram/responsive/web_screen_layout.dart';
 import 'package:instagram/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDWK_Qw_2b2zWI__44TiNH8dpzzBXEIOzg",
+            authDomain: "flutter-instagram-56d2e.firebaseapp.com",
+            projectId: "flutter-instagram-56d2e",
+            storageBucket: "flutter-instagram-56d2e.appspot.com",
+            messagingSenderId: "458086367511",
+            appId: "1:458086367511:web:54a89c0596d563c3653752"));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
